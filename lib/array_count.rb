a22c2d04e6e03113e5d3e0ce33d079e2216c7288
Gregory A.S. Wright <geslim@gmail.com>
2020-06-30 14:15:36 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -2,12 +2,10 @@ def count_strings(array)
   array.count do |element|
     element.class == String
   end
-
-
 end
 
 def count_empty_strings(array)
   array.count do |element|
-    element.class == String
-end
+    element.class == ""
+  end
 end
